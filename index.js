7eb76ac8242021758bbcf61e5447d878f7a540df
sara-yaa <57491957+sarayaari@users.noreply.github.com>
2023-05-14 12:42:17 +0300

    proj dell 3 to ep exist

---
+++ index.js
@@ -0,0 +1,7 @@
+window.onload=()=>{
+    document.querySelector("h1").style.textDecoration="underLine"
+}
+ function f1(){
+    alert ( "what you save  when i Doras what onother one  write in this line ");
+ }
+ 
